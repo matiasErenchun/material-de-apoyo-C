7874c2691378c7eb8670e64b10e61aeb4efaaac5
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2022-11-15 21:27:28 -0300

    leer archivos en C

--- apuntesunidad3/main.c
+++ apuntesunidad3/main.c
@@ -3,6 +3,27 @@
 
 //leer achivos http://ingenieriainversa.org/2017/02/14/leer-un-fichero-en-c/
 int main() {
-    printf("Hello, World!\n");
+
+    FILE *file;
+
+    char caracteres[255];
+    char *resultado;
+
+    file= fopen("..\\cosas.txt","rt");
+
+    if (file == NULL)
+    {
+        printf("error in fopen");
+    }
+    else
+    {
+        resultado = fgets(caracteres,255,file);
+        while (resultado!=NULL)
+        {
+            printf("%s",caracteres);
+            resultado = fgets(caracteres,255,file);
+        }
+    }
+
     return 0;
 }
